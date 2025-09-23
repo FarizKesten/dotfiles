089f974e11bcca40d0ddc2d5c0cd3747d2dc9256
Fariz Kesten <kestenfariz@gmail.com>
2025-09-23 23:19:56 +0200

    fix tmux vim integration

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -1,22 +1,36 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    lazy = false,
-    config = function()
-      vim.g.tmux_navigator_save_on_switch = 2
-      vim.g.tmux_navigator_disable_when_zoomed = 1
-      vim.g.tmux_navigator_no_mappings = 1 -- only if you set keys manually
-
-      vim.keymap.del("n", "<C-h>")
-      vim.keymap.del("n", "<C-j>")
-      vim.keymap.del("n", "<C-k>")
-      vim.keymap.del("n", "<C-l>")
-
-      vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", { desc = "Pane ←" })
-      vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", { desc = "Pane ↓" })
-      vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", { desc = "Pane ↑" })
-      vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", { desc = "Pane →" })
-      vim.keymap.set("n", "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", { desc = "Pane ↺" })
-    end,
+    cmd = {
+      "TmuxNavidateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Pane ←" },
+      { "<c-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Pane ↓" },
+      { "<c-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Pane ↑" },
+      { "<c-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Pane →" },
+      { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Pane ↺" },
+    },
+    -- lazy = false,
+    -- config = function()
+    --   vim.g.tmux_navigator_save_on_switch = 2
+    --   vim.g.tmux_navigator_disable_when_zoomed = 1
+    --   vim.g.tmux_navigator_no_mappings = 1 -- only if you set keys manually
+    --
+    --   vim.keymap.del("n", "<C-h>")
+    --   vim.keymap.del("n", "<C-j>")
+    --   vim.keymap.del("n", "<C-k>")
+    --   vim.keymap.del("n", "<C-l>")
+    --
+    --   vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", { desc = "Pane ←" })
+    --   vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", { desc = "Pane ↓" })
+    --   vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", { desc = "Pane ↑" })
+    --   vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", { desc = "Pane →" })
+    --   vim.keymap.set("n", "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", { desc = "Pane ↺" })
+    -- end,
   },
 }
