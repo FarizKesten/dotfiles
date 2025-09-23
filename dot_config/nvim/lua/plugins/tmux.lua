7bdf2d72c2ea41018b77fa664e23c5818838cdfd
Fariz Kesten <kestenfariz@gmail.com>
2025-09-23 23:11:13 +0200

    fix tmux issue

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -1,22 +1,22 @@
--- ~/.config/nvim/lua/tmux.lua
 return {
-  -- existing plugins…
   {
     "christoomey/vim-tmux-navigator",
-    lazy = false, -- load immediately
+    lazy = false,
     config = function()
-      -- Remap keys in Neovim for seamless navigation
-      vim.keymap.set("n", "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", { desc = "Pane ←" })
-      vim.keymap.set("n", "<C-j>", "<Cmd>TmuxNavigateDown<CR>", { desc = "Pane ↓" })
-      vim.keymap.set("n", "<C-k>", "<Cmd>TmuxNavigateUp<CR>", { desc = "Pane ↑" })
-      vim.keymap.set("n", "<C-l>", "<Cmd>TmuxNavigateRight<CR>", { desc = "Pane →" })
-      vim.keymap.set("n", "<C-\\>", "<Cmd>TmuxNavigatePrevious<CR>", { desc = "Pane ↺" })
-
-      -- Optional: auto-save buffers on switch
       vim.g.tmux_navigator_save_on_switch = 2
-
-      -- Optional: disable when a pane is zoomed
       vim.g.tmux_navigator_disable_when_zoomed = 1
+      vim.g.tmux_navigator_no_mappings = 1 -- only if you set keys manually
+
+      vim.keymap.del("n", "<C-h>")
+      vim.keymap.del("n", "<C-j>")
+      vim.keymap.del("n", "<C-k>")
+      vim.keymap.del("n", "<C-l>")
+
+      vim.keymap.set("n", "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", { desc = "Pane ←" })
+      vim.keymap.set("n", "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", { desc = "Pane ↓" })
+      vim.keymap.set("n", "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", { desc = "Pane ↑" })
+      vim.keymap.set("n", "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", { desc = "Pane →" })
+      vim.keymap.set("n", "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", { desc = "Pane ↺" })
     end,
   },
 }
